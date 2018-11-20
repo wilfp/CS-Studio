1e3e22bddf425ee7dafd5c3ee39b8d1626e29e66
wilfp <wilfaskins@gmail.com>
2018-11-20 15:37:05 +0000

    Added the profile page

--- web/javarailsite/learn/urls.py
+++ web/javarailsite/learn/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.template import loader
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+	path('profile', views.profile, name='profile'),
 ]
--- web/javarailsite/learn/views.py
+++ web/javarailsite/learn/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request):
     return HttpResponse("Hello, world. This is the learn app index.")
+
+def profile(request):
+
+	item_list = ["Item 1", "Item 2", "Item 3"];
+
+	context = {
+		"item_list" : item_list,
+    }
+	
+	template = loader.get_template("learn/profile.html")
+	
+	return HttpResponse(template.render(context, request))
