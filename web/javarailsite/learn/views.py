fbaccd335f65edd1e0a6a33b8e16f6d6b8f2ae9c
wilfp <wilfaskins@gmail.com>
2018-11-20 22:11:26 +0000

    Register the Challenge model

--- web/javarailsite/learn/views.py
+++ web/javarailsite/learn/views.py
@@ -9,15 +9,6 @@ def index(request):
 
 def profile(request):
 
-    c = Challenge(name="Hello world", level=1, path="path")
-    c.save()
-
-    c1 = Challenge(name="Create a number", level=2, path="path1")
-    c1.save()
-
-    c2 = Challenge(name="Maths wizardry", level=3, path="path1")
-    c2.save()
-
     context = {
         "challenge_list": Challenge.objects.all(),
     }
