1dbb9d281c5e4d97ca998d4e8176983dcc226896
wilfridaskins <wilfaskins@gmail.com>
2019-01-26 20:00:02 +0000

    Testing folders merged

--- javabridge/JavaBridge/src/studio/csuk/javabridge/JavaBridgeRun.java
+++ javabridge/JavaBridge/src/studio/csuk/javabridge/JavaBridgeRun.java
@@ -6,7 +6,7 @@ public class JavaBridgeRun {
 
     public static void main(String[] args) {
 
-        JavaBridge bridge = new JavaBridge(new File("work/"));
+        JavaBridge bridge = new JavaBridge(new File("temp/"));
         bridge.startCLI();
     }
 
--- javabridge/JavaBridge/temp/test.java
+++ javabridge/JavaBridge/temp/test.java
@@ -2,7 +2,16 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("Hello Hello");
+
+        System.out.println("Hello");
+        System.out.println("This is");
+        System.out.println("A more");
+
+        for(int i = 0; i < 3; i++){
+            System.out.println("complex");
+        }
+
+        System.out.println("Test");
     }
 
 }
