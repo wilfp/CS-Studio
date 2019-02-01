492675402f5b00acce956b98daf2050db7596123
wilfridaskins <wilfaskins@gmail.com>
2019-02-01 17:43:44 +0000

    JavaBridge error output added

--- javabridge/JavaBridge/src/studio/csuk/javabridge/RunJavaBridge.java
+++ javabridge/JavaBridge/src/studio/csuk/javabridge/RunJavaBridge.java
@@ -1,10 +1,26 @@
 package studio.csuk.javabridge;
 
-import java.io.File;
+import java.io.*;
 
 public class RunJavaBridge {
 
     public static void main(String[] args) {
+
+        File errorLog = new File("error.log");
+
+        if(!errorLog.exists()) {
+            try {
+                errorLog.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try (PrintStream errorStream = new PrintStream(new FileOutputStream(errorLog))) {
+            System.setErr(errorStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         JavaBridge bridge = new JavaBridge(new File(args[0]));
         bridge.startCLI();
