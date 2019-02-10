f04d6fd8774a70b788711db11619079682c0aee8
wilfridaskins <wilfaskins@gmail.com>
2019-02-10 00:06:51 +0000

    JavaBridge compile time bug fixed

--- web/javarailsite/temp/test.java
+++ web/javarailsite/temp/test.java
@@ -16,7 +16,7 @@
  *
  ******************************************************************************/
 
-public class HelloWorld {
+public class Test {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
