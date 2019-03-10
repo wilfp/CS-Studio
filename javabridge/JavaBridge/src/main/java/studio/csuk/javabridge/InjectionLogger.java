7db94d5a78d567c0afe7475348ed62a8b6f6c1d6
wilfridaskins <wilfaskins@gmail.com>
2019-03-10 15:04:18 +0000

    Variable watch framework added

--- javabridge/JavaBridge/src/main/java/studio/csuk/javabridge/InjectionLogger.java
+++ javabridge/JavaBridge/src/main/java/studio/csuk/javabridge/InjectionLogger.java
@@ -7,9 +7,11 @@ import java.util.Map;
 public class InjectionLogger {
 
     private Map<String, LinkedList<Integer>> lines;
+    private Map<String, Integer> runTiming;
 
     public InjectionLogger(){
         this.lines = new HashMap<>();
+        this.runTiming = new HashMap<>();
     }
 
     public void register(String serialName) {
@@ -27,13 +29,29 @@ public class InjectionLogger {
     @SuppressWarnings("unused")
     public void onLine(String serialName, int line){
         this.lines.get(serialName).add(line);
+        this.runTiming.putIfAbsent(serialName, 0);
+        this.runTiming.compute(serialName, (s,i) -> i+1);
     }
+
+    public void onVariableInit(String serialName, String variableName, int scope, Object data){
+
+    }
+
 
     public String getLineCode(String serialName, int line, boolean offset){
 
         if(offset) line -= 1;
 
         return "studio.csuk.javabridge.InjectionLogger.get().onLine(\"" + serialName + "\", " + line + ");";
+    }
+
+    public String getVariableInitCode(String serialName, String variableName, int scope, Object data) {
+
+        return String.format("studio.csuk.javabridge.InjectionLogger.get().onVariableInit(%s,%s,%s,%s)", serialName, variableName, scope, data);
+    }
+
+    public String getVariableAssignCode(String serialName, int variableID, Object value) {
+        return String.format("studio.csuk.javabridge.InjectionLogger.get().onVariableAssign(%s,%s,%s)", serialName, variableID, value);
     }
 
     private static final InjectionLogger instance = new InjectionLogger();
--- javabridge/JavaBridge/src/main/java/studio/csuk/javabridge/JavaRunLog.java
+++ javabridge/JavaBridge/src/main/java/studio/csuk/javabridge/JavaRunLog.java
@@ -1,7 +1,9 @@
 package com.wilfaskins.javabridge;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class JavaRunLog {
 
@@ -14,4 +16,18 @@ public class JavaRunLog {
     public void add(String entry){
         this.entries.add(entry);
     }
+
+    class Variable<T>{
+
+        private String name;
+        private int scope;
+        private Map<Integer, T> values;
+
+        public Variable(String name, int scope){
+            this.name = name;
+            this.scope = scope;
+            values = new HashMap<>();
+        }
+    }
+
 }
