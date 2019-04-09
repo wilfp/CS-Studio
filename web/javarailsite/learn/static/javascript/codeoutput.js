7f066968f72dc8906d5f525b25b876c9a7b8ab73
wilfridaskins <wilfaskins@gmail.com>
2019-04-09 16:07:16 +0100

    codeoutput.js formatted correctly

--- web/javarailsite/learn/static/javascript/codeoutput.js
+++ web/javarailsite/learn/static/javascript/codeoutput.js
@@ -1,4 +1,3 @@
-
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -29,23 +28,23 @@ $.ajaxSetup({
     }
 });
 
-    Vue.component('output-panel', {
-      
-      props: [ "className", "content" ],
-      
-      template: '<div :class="className"> {{ content }} </div>'
-      
-    });
-    
-    Vue.component('text-transition', {
-        
-      data: function () {
-          return {
-              displayTextIndex: 0,
-              displayText: null
-          }
-      },
-      template: `
+Vue.component('output-panel', {
+
+    props: ["className", "content"],
+
+    template: '<div :class="className"> {{ content }} </div>'
+
+});
+
+Vue.component('text-transition', {
+
+    data: function() {
+        return {
+            displayTextIndex: 0,
+            displayText: null
+        }
+    },
+    template: `
       <transition
       appear
       mode="out-in"
@@ -56,123 +55,130 @@ $.ajaxSetup({
       <slot></slot>
       </transition>
       `,
-      methods: {
-                    
-          onBeforeTextAppear: function (el) {
-              displayText = el.innerHTML;
-              displayTextIndex = 0;
-              el.innerHTML = "";
-          },
-          
-          onTextAppear: function (el, done) {
-              displayTextStep(el);
-              done();
-          }
-          
+    methods: {
+
+        onBeforeTextAppear: function(el) {
+            displayText = el.innerHTML;
+            displayTextIndex = 0;
+            el.innerHTML = "";
+        },
+
+        onTextAppear: function(el, done) {
+            displayTextStep(el);
+            done();
         }
 
-    });
-    
-function displayTextStep(el){
-              
-          if(displayTextIndex <= displayText.length){
-              var nextChar = displayText.charAt(displayTextIndex);
-              el.innerHTML += nextChar;
-              displayTextIndex++;
-              setTimeout(function(){ displayTextStep(el) }, 30);
-          }
-      }
+    }
+
+});
+
+function displayTextStep(el) {
+
+    if (displayTextIndex <= displayText.length) {
+        var nextChar = displayText.charAt(displayTextIndex);
+        el.innerHTML += nextChar;
+        displayTextIndex++;
+        setTimeout(function() {
+            displayTextStep(el)
+        }, 30);
+    }
+}
 
 var app = new Vue({
-      el: '#app',
-	  delimiters: ['[[', ']]'],
-      data:{
-          outputs: []
-      }
-	});
+    el: '#app',
+    delimiters: ['[[', ']]'],
+    data: {
+        outputs: []
+    }
+});
 
 var codeMirror;
 
-function onLoad(){
-        
+function onLoad() {
+
     var has_indent = document.getElementById("has-indent") == "true";
-    
+
     codeMirror = CodeMirror.fromTextArea(document.getElementById("code-area"), {
-    lineNumbers: true,
-    matchBrackets: true,
-    mode: "text/x-java",
-    theme: "eclipse",
-    smartIndent: has_indent
-  });
-  
-  codeMirror.setSize(null, 500);
-  
-  codeMirror.on("change", function(){ codeChanged = true; });        
+        lineNumbers: true,
+        matchBrackets: true,
+        mode: "text/x-java",
+        theme: "eclipse",
+        smartIndent: has_indent
+    });
+
+    codeMirror.setSize(null, 500);
+
+    codeMirror.on("change", function() {
+        codeChanged = true;
+    });
 }
 
 // Called when the run button is clicked
 function runCodeFunction() {
-    
+
     // If no changes
-    if(!codeChanged){
+    if (!codeChanged) {
         startLineAnimation();
         return;
     }
-    
-	var codeArea = codeMirror.getValue();
-	codeArea = window.btoa(codeArea);
-	
-	var result = sendPost(codeArea);
+
+    var codeArea = codeMirror.getValue();
+    codeArea = window.btoa(codeArea);
+
+    var result = sendPost(codeArea);
 }
 
-function getChallengeID(){
-	return $("#challengeID").html();
+function getChallengeID() {
+    return $("#challengeID").html();
 }
 
-function setLoading(status){
-    
-    if(status){
+function setLoading(status) {
+
+    if (status) {
         $("#button-play-code").addClass("is-loading");
-    }else{
+    } else {
         $("#button-play-code").removeClass("is-loading");
     }
 }
 
 function sendPost(code) {
-	
-	setLoading(true);
-	
-	var challengeID = getChallengeID();
-	var jsonData = '{ \"challenge_id\": \"' + challengeID  + '\", \"code\": \"' + code + '\" }';
-	
+
+    setLoading(true);
+
+    var challengeID = getChallengeID();
+    var jsonData = '{ \"challenge_id\": \"' + challengeID + '\", \"code\": \"' + code + '\" }';
+
     $.ajax({
         type: "POST",
         url: '/learn/runJavaCode',
-		dataType: 'json',
+        dataType: 'json',
         data: jsonData,
         success: response
     });
 
 }
 
-function response(msg, status, jqXHR) {    
-    
+function response(msg, status, jqXHR) {
+
     app.$data.outputs = [];
-        
+
     var rawText = msg["text"];
     var textLines = rawText.split("\n");
     var statusClass = getStatus(msg["status"]);
-    
-    for(var i = 0; i < textLines.length; i++){
+
+    for (var i = 0; i < textLines.length; i++) {
         var line = textLines[i];
-        app.$data.outputs.push( { content: line, className: statusClass } );
+        app.$data.outputs.push({
+            content: line,
+            className: statusClass
+        });
     }
-    
+
     lines = msg["lines"];
     codeChanged = false;
     startLineAnimation();
-    
-	setLoading(false);
+
+    setLoading(false);
 }
 
 // The current lines being displayed
@@ -182,36 +188,36 @@ var shouldRun = false;
 var codeChanged = true;
 
 // Goes through all lines, highlighting each in turn
-function startLineAnimation(){
+function startLineAnimation() {
     shouldRun = true;
     currentLine = 0;
     highlightNextLine();
 }
 
 // Called recursively to highlight each line in turn
-function highlightNextLine(){
-    
+function highlightNextLine() {
+
     // Highlight current line
     highlightLine(lines[currentLine]);
     // Get next line
     currentLine++;
-    
+
     // If lines left and not stopped
-    if(currentLine < lines.length && shouldRun){
+    if (currentLine < lines.length && shouldRun) {
         // Schedule next line
         setTimeout(highlightNextLine, 250);
-    }else{
+    } else {
         // Clear last line
         lastMarker.clear();
     }
 }
 
-function getStatus(status){
-    
-    if(status == "SUCCESS"){
+function getStatus(status) {
+
+    if (status == "SUCCESS") {
         return "cs-box dark-blue";
     }
-    
+
     return "cs-box";
 }
 
@@ -219,18 +225,26 @@ function getStatus(status){
 var lastMarker = null;
 
 // Highlights one line of the code area
-function highlightLine(x){
-    
-    if(lastMarker != null){
+function highlightLine(x) {
+
+    if (lastMarker != null) {
         lastMarker.clear();
     }
-    
-    lastMarker = codeMirror.markText({line: x, ch: 0}, {line: x+1, ch: 0}, {className: "highlight"});
+
+    lastMarker = codeMirror.markText({
+        line: x,
+        ch: 0
+    }, {
+        line: x + 1,
+        ch: 0
+    }, {
+        className: "highlight"
+    });
 }
 
-$(document).ready(function(){
-    
+$(document).ready(function() {
+
     $("#button-play-code").click(runCodeFunction);
-    
-	onLoad();
+
+    onLoad();
 });
