5e1a6d8aab504e67a1eaf55c15e3c8e3f84a0ca8
wilfridaskins <wilfaskins@gmail.com>
2019-04-09 15:39:24 +0100

    Transition test completed

--- styling/transition_test.js
+++ styling/transition_test.js
@@ -1,31 +1,15 @@
 
-$(document).ready(function(){
-
 
 new Vue({
-  el: '#example-4',
+  el: '#app',
   data: {
     show: false
   },
   methods: {
-    beforeEnter: function (el) {
-      el.style.opacity = 0
+    appear: function (el, done) {
+        alert("test");
+        el.innerHTML = "<div>WORKING</div>";
+      done();
     },
-    enter: function (el, done) {
-      Velocity(el, { opacity: 1, fontSize: '1.4em' }, { duration: 300 })
-      Velocity(el, { fontSize: '1em' }, { complete: done })
-    },
-    leave: function (el, done) {
-      Velocity(el, { translateX: '15px', rotateZ: '50deg' }, { duration: 600 })
-      Velocity(el, { rotateZ: '100deg' }, { loop: 2 })
-      Velocity(el, {
-        rotateZ: '45deg',
-        translateY: '30px',
-        translateX: '30px',
-        opacity: 0
-      }, { complete: done })
-    }
   }
-});
-
 });
