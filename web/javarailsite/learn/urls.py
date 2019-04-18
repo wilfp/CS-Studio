6ec28a8b8a6dff4e6ecdd60eaa092103b537bd78
wilfp <wilfaskins@gmail.com>
2019-04-18 22:20:51 +0100

    Page listings updated

--- web/javarailsite/learn/urls.py
+++ web/javarailsite/learn/urls.py
@@ -6,7 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('profile', views.profile, name='profile'),
-    path('progress', views.progress, name='progress'),
+    path('progress', views.progress, name='progress`'),
     path('challenge/<int:challenge_id>/', views.challenge, name='challenge'),
     path('runJavaCode', views.run_java_code, name='runJavaCode'),
+    
+    path('blog', views.blog, name='blog'),
+    path('login', views.login, name='login'),
 ]
